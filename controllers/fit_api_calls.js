e3a9f3a276b40f62039f4fac1cabca98d047eb98
bradschillschool@gmail.com <bradschillschool@gmail.com>
2022-08-08 12:56:20 -0500

    working on the workout api calls

--- controllers/fit_api_calls.js
+++ controllers/fit_api_calls.js
@@ -1,16 +1,16 @@
 const axios = require("axios");
 require('dotenv').config();
 
+//-------------------------------list of exercises----------------------------
 
 // const options = {
 //   method: 'GET',
 //   url: 'https://exercisedb.p.rapidapi.com/exercises',
 //   headers: {
-//     'X-RapidAPI-Key': 'FIT_DB_KEY',
+//     'X-RapidAPI-Key': process.env.FIT_DB_KEY,
 //     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
 //   }
 // };
-
 
 // axios.request(options).then(function (response) {
 // 	console.log(response.data);
@@ -18,19 +18,141 @@ require('dotenv').config();
 // 	console.error(error);
 // });
 
-// console.log(process.env.FIT_DB_KEY)
+//-----------------------------list of body parts------------------------------
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+//   headers: {
+//     'X-RapidAPI-Key': 'process.env.FIT_DB_KEY',
+//     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+//---------------------------list of target muscles----------------------------
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://exercisedb.p.rapidapi.com/exercises/targetList',
+//   headers: {
+//     'X-RapidAPI-Key': process.env.FIT_DB_KEY,
+//     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+//---------------------------list of equipment----------------------------------
+
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://exercisedb.p.rapidapi.com/exercises/equipmentList',
+//   headers: {
+//     'X-RapidAPI-Key': process.env.FIT_DB_KEY,
+//     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+//---------------------------list by body part--------------------------------
+
+const bodypart = ['back','cardio','chest','lower arms','lower legs','neck','shoulders','upper arms','upper legs','waist']
+
+//'upper arms' url 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/upper%20arms'
+//'chest' url 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/chest'
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/%7BbodyPart%7D',
+//   headers: {
+//     'X-RapidAPI-Key': process.env.FIT_DB_KEY,
+//     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+//---------------------------list by target muscle------------------------------
+
+ const targetMuscle = ["abductors","abs","adductors","biceps","calves","cardiovascular system","delts","forearms","glutes","hamstrings","lats","levator scapulae","pectorals","quads","serratus anterior","spine","traps","triceps","upper back"]
+
+// // 'levator scapulae' url 'https://exercisedb.p.rapidapi.com/exercises/target/levator%20scapulae'
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://exercisedb.p.rapidapi.com/exercises/target/%7Btarget%7D',
+//   headers: {
+//     'X-RapidAPI-Key': process.env.FIT_DB_KEY,
+//     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+//--------------------------------list by equipment-----------------------------------
+
+const equipment = ["assisted","band","barbell","body weight","bosu ball","cable","dumbbell","elliptical machine","ez barbell","hammer","kettlebell","leverage machine",
+"medicine ball","olympic barbell","resistance band","roller","rope","skierg machine","sled machine","smith machine","stability ball","stationary bike",
+"stepmill machine","tire","trap bar","upper body ergometer","weighted","wheel roller"]
+
+// 'upper body ergometer' url 'https://exercisedb.p.rapidapi.com/exercises/equipment/upper%20body%20ergometer'
+
+function formatSelection (selection) {
+    let new_string = selection.replace(/ /g,'%20')
+    return new_string
+}
+
+// let string = 'https://exercisedb.p.rapidapi.com/exercises/equipment/'
+
+// console.log(string.concat('',formatSelection(equipment[25])))
+
+
+
+
+
 
 const options = {
   method: 'GET',
-  url: 'https://exercisedb.p.rapidapi.com/exercises/targetList',
+  url: 'https://exercisedb.p.rapidapi.com/exercises/equipment/',
   headers: {
     'X-RapidAPI-Key': process.env.FIT_DB_KEY,
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 };
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+
+console.log(options.url)
+
+
+
+
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
