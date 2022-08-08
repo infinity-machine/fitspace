a1cdee51e07f48cee4bf3c0ef0638a4fa05d890e
bradschillschool@gmail.com <bradschillschool@gmail.com>
2022-08-08 14:03:06 -0500

    seeing if tailwinds is going to work

--- controllers/fit_api_calls.js
+++ controllers/fit_api_calls.js
@@ -1,5 +1,5 @@
-const axios = require("axios");
-require('dotenv').config();
+// const axios = require("axios");
+// require('dotenv').config();
 
 //-------------------------------list of exercises----------------------------
 
@@ -72,7 +72,7 @@ require('dotenv').config();
 
 //---------------------------list by body part--------------------------------
 
-const bodypart = ['back','cardio','chest','lower arms','lower legs','neck','shoulders','upper arms','upper legs','waist']
+// const bodypart = ['back','cardio','chest','lower arms','lower legs','neck','shoulders','upper arms','upper legs','waist']
 
 //'upper arms' url 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/upper%20arms'
 //'chest' url 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/chest'
@@ -94,7 +94,7 @@ const bodypart = ['back','cardio','chest','lower arms','lower legs','neck','shou
 
 //---------------------------list by target muscle------------------------------
 
- const targetMuscle = ["abductors","abs","adductors","biceps","calves","cardiovascular system","delts","forearms","glutes","hamstrings","lats","levator scapulae","pectorals","quads","serratus anterior","spine","traps","triceps","upper back"]
+//  const targetMuscle = ["abductors","abs","adductors","biceps","calves","cardiovascular system","delts","forearms","glutes","hamstrings","lats","levator scapulae","pectorals","quads","serratus anterior","spine","traps","triceps","upper back"]
 
 // // 'levator scapulae' url 'https://exercisedb.p.rapidapi.com/exercises/target/levator%20scapulae'
 
@@ -115,16 +115,16 @@ const bodypart = ['back','cardio','chest','lower arms','lower legs','neck','shou
 
 //--------------------------------list by equipment-----------------------------------
 
-const equipment = ["assisted","band","barbell","body weight","bosu ball","cable","dumbbell","elliptical machine","ez barbell","hammer","kettlebell","leverage machine",
-"medicine ball","olympic barbell","resistance band","roller","rope","skierg machine","sled machine","smith machine","stability ball","stationary bike",
-"stepmill machine","tire","trap bar","upper body ergometer","weighted","wheel roller"]
+// const equipment = ["assisted","band","barbell","body weight","bosu ball","cable","dumbbell","elliptical machine","ez barbell","hammer","kettlebell","leverage machine",
+// "medicine ball","olympic barbell","resistance band","roller","rope","skierg machine","sled machine","smith machine","stability ball","stationary bike",
+// "stepmill machine","tire","trap bar","upper body ergometer","weighted","wheel roller"]
 
 // 'upper body ergometer' url 'https://exercisedb.p.rapidapi.com/exercises/equipment/upper%20body%20ergometer'
 
-function formatSelection (selection) {
-    let new_string = selection.replace(/ /g,'%20')
-    return new_string
-}
+// function formatSelection (selection) {
+//     let new_string = selection.replace(/ /g,'%20')
+//     return new_string
+// }
 
 // let string = 'https://exercisedb.p.rapidapi.com/exercises/equipment/'
 
@@ -132,23 +132,18 @@ function formatSelection (selection) {
 
 
 
+// const options = {
+//   method: 'GET',
+//   url: 'https://exercisedb.p.rapidapi.com/exercises/equipment/',
+//   headers: {
+//     'X-RapidAPI-Key': process.env.FIT_DB_KEY,
+//     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+//   }
+// };
 
 
-
-const options = {
-  method: 'GET',
-  url: 'https://exercisedb.p.rapidapi.com/exercises/equipment/',
-  headers: {
-    'X-RapidAPI-Key': process.env.FIT_DB_KEY,
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-  }
-};
-
-
-console.log(options.url)
-
-
-
+// console.log(options.url)
+// console.log(string.concat('',formatSelection(equipment[25])))
 
 
 // axios.request(options).then(function (response) {
@@ -156,3 +151,54 @@ console.log(options.url)
 // }).catch(function (error) {
 // 	console.error(error);
 // });
+
+//-----------------------------------------------SANDBOX-------------------------------------------------------
+const axios = require("axios");
+require('dotenv').config();
+
+
+const equipment = ["assisted","band","barbell","body weight","bosu ball","cable","dumbbell","elliptical machine","ez barbell","hammer","kettlebell","leverage machine",
+"medicine ball","olympic barbell","resistance band","roller","rope","skierg machine","sled machine","smith machine","stability ball","stationary bike",
+"stepmill machine","tire","trap bar","upper body ergometer","weighted","wheel roller"] //28 items
+
+const targetMuscle = ["abductors","abs","adductors","biceps","calves","cardiovascular system","delts","forearms","glutes","hamstrings","lats","levator scapulae",
+"pectorals","quads","serratus anterior","spine","traps","triceps","upper back"] //19 items
+
+const bodypart = ['back','cardio','chest','lower arms','lower legs','neck','shoulders','upper arms','upper legs','waist'] //10 items
+
+
+function formatSelection (selection) {
+    let new_string = selection.replace(/ /g,'%20')
+    return new_string
+}
+
+//list of exercises   url: 'https://exercisedb.p.rapidapi.com/exercises',
+//by target muscles   url: 'https://exercisedb.p.rapidapi.com/exercises/target/',
+//by body part        url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/',
+//by equipment        url: 'https://exercisedb.p.rapidapi.com/exercises/equipment/',
+
+
+const options = {
+    method: 'GET',
+    url: 'https://exercisedb.p.rapidapi.com/exercises',
+    headers: {
+      'X-RapidAPI-Key': process.env.FIT_DB_KEY,
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+
+//   axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+
+
+function testfunc (api_info) {
+    axios.request(api_info).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
+}
