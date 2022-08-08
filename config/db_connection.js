46ae7d97ddaae33d10ecc09fcfa40988b06306bc
cr7st74n <cris-082011@hotmail.com>
2022-08-08 16:07:42 -0500

    setting up materialize and sidebar

--- config/db_connection.js
+++ config/db_connection.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, 
+const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, "", 
   {
     host: 'localhost',
     dialect: 'mysql', 
