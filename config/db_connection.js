ffc465cce6960fb84c71e41e38a6917f92ec46e9
bradschillschool@gmail.com <bradschillschool@gmail.com>
2022-08-10 08:54:21 -0500

    heroku ready?

--- config/db_connection.js
+++ config/db_connection.js
@@ -10,3 +10,26 @@ const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proce
 );
 
 module.exports = connection;
+
+//------------------------------old above-------------------------------
+
+// const mysql = require('mysql2');
+// const is_production = process.env.NODE_ENV === 'production';
+
+// const connection_data = is_production ? {
+
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+// } : {
+
+//   host: 'localhost',
+//   database: 'fitspace_db',
+//   user: 'root',
+//   password: '',
+// }
+
+// const connection = mysql.createPool(connection_data)
+
+// module.exports = connection;
